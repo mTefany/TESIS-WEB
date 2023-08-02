c1d5cf6fc6dd85f81f7ca65e42f4aa2ffefca776
mTefany <maciaspicostefany@gmail.com>
2023-08-02 10:29:17 -0500

    notify en el tablero

--- src/components/Tablero.js
+++ src/components/Tablero.js
@@ -4,35 +4,40 @@ import Area from '../hook/Area';
 import Pastel from '../hook/Pastel';
 import Radial from '../hook/Radial';
 import Nav from '../partials/Nav';
+import Notify from './Notificacion';
 
 function Tablero() {
   return (
     <div>
       <Nav />
+      <Notify />
       <div className="container bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4 mt-3">
         <h4 className="text-xl mb-4 text-uppercase">
           Bienvenido al tablero de control.
         </h4>
-        <div className="grid grid-cols-2 gap-4">
-          {/* Contenedor de Area y Barras */}
-          <div style={{ display: 'flex' }}>
-            <div style={{ flex: 1 }}>
-              <Area />
+        <div className='grip grap-4'>
+          <div className="grid md:flex-row gap-4">
+            {/* Contenedor de Area y Barras */}
+            <div className='grid md:flex-row gap-4' style={{ display: 'flex' }}>
+              <div style={{ flex: 1 }}>
+                <Area />
+              </div>
+              <div style={{ flex: 1 }}>
+                <Barras />
+              </div>
             </div>
-            <div style={{ flex: 1 }}>
-              <Barras />
-            </div>
-          </div>
-          {/* Contenedor de Pastel y Radial */}
-          <div style={{ display: 'flex' }}>
-            <div style={{ flex: 1 }}>
-              <Pastel />
-            </div>
-            <div style={{ flex: 1 }}>
-              <Radial />
+            {/* Contenedor de Pastel y Radial */}
+            <div className='grid md:flex-row gap-4' style={{ display: 'flex' }}>
+              <div style={{ flex: 1 }}>
+                <Pastel />
+              </div>
+              <div style={{ flex: 1 }}>
+                <Radial />
+              </div>
             </div>
           </div>
         </div>
+
       </div>
     </div>
   );
